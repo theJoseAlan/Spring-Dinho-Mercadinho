ffb777377f03002aaed7c3208c860a36e9520a66
theJoseAlan <salesalan854@gmail.com>
2023-04-17 18:11:45 -0300

    Initial commit

---
+++ src/main/java/br/com/bentinho/bentinhoMercadinho/BentinhoMercadinhoApplication.java
@@ -0,0 +1,13 @@
+package br.com.bentinho.bentinhoMercadinho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BentinhoMercadinhoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BentinhoMercadinhoApplication.class, args);
+	}
+
+}
