e5ffc59264ee324dc56911b8b7fa2ef9befc23f9
theJoseAlan <salesalan854@gmail.com>
2023-04-19 14:48:03 -0300

    Create Carrinho.java

---
+++ src/main/java/br/com/bentinho/mercadinho/entity/Carrinho.java
@@ -0,0 +1,49 @@
+package br.com.bentinho.mercadinho.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Carrinho {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    private Comprador comprador;
+
+    @OneToMany
+    private List<Produto> produtos;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Comprador getComprador() {
+        return comprador;
+    }
+
+    public void setComprador(Comprador comprador) {
+        this.comprador = comprador;
+    }
+
+    public List<Produto> getProdutos() {
+        return produtos;
+    }
+
+    public void setProdutos(List<Produto> produtos) {
+        this.produtos = produtos;
+    }
+
+}
