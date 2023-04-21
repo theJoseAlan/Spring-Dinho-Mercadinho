e3aac4d2c07ce80a3acdf94ee13296eaefeb595b
theJoseAlan <salesalan854@gmail.com>
2023-04-21 13:24:43 -0300

    Create ProdutoDto.java

---
+++ src/main/java/br/com/bentinho/mercadinho/entity/dto/ProdutoDto.java
@@ -0,0 +1,13 @@
+package br.com.bentinho.mercadinho.entity.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ProdutoDto {
+
+    private Long id;
+    private String nome;
+
+}
