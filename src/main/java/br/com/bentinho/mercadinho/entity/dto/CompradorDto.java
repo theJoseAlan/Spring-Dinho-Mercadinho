1e7372e7826e24bcb4f054b216bb907e83919b76
theJoseAlan <salesalan854@gmail.com>
2023-04-21 13:24:41 -0300

    Create CompradorDto.java

---
+++ src/main/java/br/com/bentinho/mercadinho/entity/dto/CompradorDto.java
@@ -0,0 +1,13 @@
+package br.com.bentinho.mercadinho.entity.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CompradorDto {
+
+    private Long id;
+    private String nome;
+
+}
