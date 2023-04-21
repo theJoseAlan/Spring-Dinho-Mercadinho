53ec47ae05b96eaa05653c80dc83a0f052a8b0a4
theJoseAlan <salesalan854@gmail.com>
2023-04-21 13:24:39 -0300

    Update Comprador.java

--- src/main/java/br/com/bentinho/mercadinho/entity/Comprador.java
+++ src/main/java/br/com/bentinho/mercadinho/entity/Comprador.java
@@ -2,9 +2,11 @@ package br.com.bentinho.mercadinho.entity;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Getter
 @Setter
 @AllArgsConstructor
@@ -12,6 +14,7 @@ import lombok.Setter;
 public class Comprador {
 
 
+    @EqualsAndHashCode.Include
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
