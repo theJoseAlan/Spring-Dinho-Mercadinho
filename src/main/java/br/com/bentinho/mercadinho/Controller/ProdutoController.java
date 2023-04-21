63bbb9b27929f370017d99c0ce23d7097a1dc771
theJoseAlan <salesalan854@gmail.com>
2023-04-21 13:24:32 -0300

    Update ProdutoController.java

--- src/main/java/br/com/bentinho/mercadinho/Controller/ProdutoController.java
+++ src/main/java/br/com/bentinho/mercadinho/Controller/ProdutoController.java
@@ -1,15 +1,20 @@
 package br.com.bentinho.mercadinho.Controller;
 
 import br.com.bentinho.mercadinho.entity.Produto;
+import br.com.bentinho.mercadinho.repository.ProdutoRepository;
 import br.com.bentinho.mercadinho.service.ProdutoService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @AllArgsConstructor
 @RestController
 @RequestMapping("/produto")
 public class ProdutoController {
+
+    private ProdutoRepository repository;
 
     private ProdutoService service;
 
@@ -17,6 +22,11 @@ public class ProdutoController {
     @ResponseStatus(HttpStatus.CREATED)
     public Produto adicionar(@RequestBody Produto produto) {
         return service.save(produto);
+    }
+
+    @GetMapping("/listar")
+    public List<Produto> listagem(){
+        return repository.findAll();
     }
 
 }
