ab82703a65247087782edd1ad9e072389706b44b
theJoseAlan <salesalan854@gmail.com>
2023-04-23 11:05:45 -0300

    Atributo comprador em Carrinho

--- src/main/java/br/com/bentinho/mercadinho/Controller/CarrinhoController.java
+++ src/main/java/br/com/bentinho/mercadinho/Controller/CarrinhoController.java
@@ -1,7 +1,9 @@
 package br.com.bentinho.mercadinho.Controller;
 
 import br.com.bentinho.mercadinho.entity.Carrinho;
+import br.com.bentinho.mercadinho.entity.Comprador;
 import br.com.bentinho.mercadinho.repository.CarrinhoRepository;
+import br.com.bentinho.mercadinho.repository.CompradorRepository;
 import br.com.bentinho.mercadinho.service.CarrinhoService;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -17,9 +19,19 @@ public class CarrinhoController {
 
     private CarrinhoRepository carrinhoRepository;
 
+    private final CompradorRepository compradorRepository;
+
     @PostMapping
-    public Carrinho criar(Carrinho carrinho){
-        return carrinhoService.save(carrinho);
+    public Carrinho criar(@RequestBody Carrinho carrinho){
+
+        Comprador comprador = carrinho.getComprador();
+        if (comprador.getId() != null) { // Verifica se a entidade Pessoa já possui ID atribuído
+            comprador = compradorRepository.findById(comprador.getId()).orElse(null); // Recupera a entidade Pessoa do banco de dados
+        }
+        carrinho.setComprador(comprador); // Associa a entidade Pessoa com Casa
+
+        return carrinhoRepository.save(carrinho);
+
     }
 
     @GetMapping("/{id}")
