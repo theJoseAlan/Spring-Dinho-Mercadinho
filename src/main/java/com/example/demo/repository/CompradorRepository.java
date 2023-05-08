532b0b52b9be5213a4bf89a2d94882058525ca0e
theJoseAlan <salesalan854@gmail.com>
2023-05-08 13:56:30 -0300

    Create CompradorRepository.java

---
+++ src/main/java/com/example/demo/repository/CompradorRepository.java
@@ -0,0 +1,10 @@
+package com.example.demo.repository;
+
+import com.example.demo.entidades.Comprador;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CompradorRepository extends JpaRepository<Comprador, Long> {
+
+}
