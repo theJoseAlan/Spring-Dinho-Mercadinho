3447c2964e54cf411c685b608143de085b8485a5
theJoseAlan <salesalan854@gmail.com>
2023-05-08 13:56:24 -0300

    List for id, delete and put

--- src/main/java/com/example/demo/controller/CompradorController.java
+++ src/main/java/com/example/demo/controller/CompradorController.java
@@ -1,22 +1,24 @@
 package com.example.demo.controller;
 
 import com.example.demo.entidades.Comprador;
-import com.example.demo.repository.PessoaRepository;
+import com.example.demo.repository.CompradorRepository;
 import com.example.demo.service.CompradorService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @AllArgsConstructor
 @RestController
-@RequestMapping("/pessoa")
+@RequestMapping("/comprador")
 public class CompradorController {
 
     private CompradorService compradorService;
 
-    private PessoaRepository pessoaRepository;
+    private CompradorRepository compradorRepository;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
@@ -26,7 +28,29 @@ public class CompradorController {
 
     @GetMapping("/exibir")
     public List<Comprador> exibir(){
-        return pessoaRepository.findAll();
+        return compradorRepository.findAll();
+    }
+
+
+    @GetMapping("/{id}")
+    public Optional<Comprador> exibeporId(@PathVariable Long id){
+        return compradorRepository.findById(id);
+    }
+
+    @PutMapping("{id}")
+    public Comprador atualizar(@PathVariable Long id, @RequestBody Comprador comprador){
+
+        comprador.setId(id);
+
+        return compradorService.salvar(comprador);
+
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<Void> deletar(@PathVariable Long id){
+        compradorRepository.deleteById(id);
+
+        return ResponseEntity.noContent().build();
     }
 
 
