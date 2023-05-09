472457470b567d12d555a56255ff17b5c91e7a62
theJoseAlan <salesalan854@gmail.com>
2023-05-09 09:25:04 -0300

    Teste para classe principal

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -7,7 +7,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 class DemoApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void main() {
+		DemoApplication.main(new String[]{});
 	}
 
 }
