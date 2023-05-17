0dc2f4edf402caf2cb716f1af87c4945d6bb681a
theJoseAlan <salesalan854@gmail.com>
2023-05-17 08:26:31 -0300

    Postmapping de Produto retorna created

--- src/main/java/com/example/demo/controller/ProdutoController.java
+++ src/main/java/com/example/demo/controller/ProdutoController.java
@@ -4,8 +4,11 @@ import com.example.demo.entidades.Produto;
 import com.example.demo.repository.ProdutoRepository;
 import com.example.demo.service.ProdutoService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.Optional;
 
 @AllArgsConstructor
@@ -18,8 +21,18 @@ public class ProdutoController {
     private ProdutoRepository produtoRepository;
 
     @PostMapping
-    public Produto adcionar(@RequestBody Produto produto){
-        return cidadeService.save(produto);
+    public ResponseEntity<Produto> adcionar(@RequestBody Produto produto){
+        Produto produtoSalvo = cidadeService.save(produto);
+
+        // Cria a URI do recurso criado
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(produtoSalvo.getId())
+                .toUri();
+
+        // Retorna a resposta com o status "Created" e a URI do recurso criado
+        return ResponseEntity.created(uri).body(produtoSalvo);
+
     }
 
     @GetMapping("/{id}")
