4da9d212dff6a7b1b6bea4880cdf457569db63d6
theJoseAlan <salesalan854@gmail.com>
2023-05-17 14:34:20 -0300

    Teste ProdutoController

--- src/test/java/com/example/demo/controller/ProdutoControllerTest.java
+++ src/test/java/com/example/demo/controller/ProdutoControllerTest.java
@@ -10,8 +10,14 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
-import static org.mockito.Mockito.any;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 @SpringBootTest
@@ -36,7 +42,6 @@ class ProdutoControllerTest {
     @BeforeEach
     void setUp(){
         MockitoAnnotations.openMocks(this);
-
         startProduto();
     }
 
@@ -44,11 +49,51 @@ class ProdutoControllerTest {
     void adcionar() {
         when(produtoService.save(any())).thenReturn(produto);
 
-        //ResponseEntity
+        ResponseEntity<Produto> response = produtoController.adcionar(produto);
+
+        assertEquals(ResponseEntity.class, response.getClass());
+        assertEquals(HttpStatus.CREATED, response.getStatusCode());
+
+        assertNotNull(response);
+
+        //Não sei se é necessário, se a próxima verificação passa é porque o ID não foi nulo
+        //Mas vou deixar como lembrete de que é interessante verificar
+        assertNotNull(response.getBody().getId());
+
+        assertEquals(ID, response.getBody().getId());
+        assertEquals(NOME, response.getBody().getNome());
+        assertEquals(VALOR_UNITARIO, response.getBody().getValorUnitario());
+        assertEquals(QUANTIDADE, response.getBody().getQuantidade());
+
+        double valorTotal = response.getBody().getQuantidade()*response.getBody().getValorUnitario();
+
+        assertEquals(QUANTIDADE*VALOR_UNITARIO, valorTotal);
+
     }
 
     @Test
     void exibe() {
+        when(produtoRepository.findById(any())).thenReturn(Optional.of(produto));
+
+        ResponseEntity<Optional<Produto>> response = produtoController.exibe(ID);
+
+        assertNotNull(response);
+        assertNotNull(response.getBody());
+
+        assertEquals(Optional.class, response.getBody().getClass());
+
+        assertEquals(ResponseEntity.class, response.getClass());
+
+        assertEquals(ID, response.getBody().get().getId());
+        assertEquals(NOME, response.getBody().get().getNome());
+        assertEquals(VALOR_UNITARIO, response.getBody().get().getValorUnitario());
+        assertEquals(QUANTIDADE, response.getBody().get().getQuantidade());
+
+        double valorTotal = response.getBody().get().getQuantidade()*response.getBody().get().getValorUnitario();
+
+        assertEquals(QUANTIDADE*VALOR_UNITARIO, valorTotal);
+
+
     }
 
     private void startProduto(){
