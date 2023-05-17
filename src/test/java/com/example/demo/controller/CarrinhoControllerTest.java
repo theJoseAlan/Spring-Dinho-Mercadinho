deb58b015f1d9d4b26fb6f1bd14498f1f01dfc2f
theJoseAlan <salesalan854@gmail.com>
2023-05-17 15:34:32 -0300

    Create CarrinhoControllerTest.java

---
+++ src/test/java/com/example/demo/controller/CarrinhoControllerTest.java
@@ -0,0 +1,16 @@
+package com.example.demo.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CarrinhoControllerTest {
+
+    @Test
+    void criar() {
+    }
+
+    @Test
+    void exibeporId() {
+    }
+}
