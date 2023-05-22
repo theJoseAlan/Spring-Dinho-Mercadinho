02359d8eac68f6304e25e15a90e58a5c894c8b94
theJoseAlan <salesalan854@gmail.com>
2023-05-22 11:14:04 -0300

    add buscar
    return produtoRepository.findById(produtoId).orElseThrow();
        }

--- src/main/java/demo/mercadinho/service/ProdutoService.java
+++ src/main/java/demo/mercadinho/service/ProdutoService.java
@@ -5,6 +5,7 @@ import demo.mercadinho.repository.ProdutoRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @AllArgsConstructor
 @Service
@@ -13,8 +14,12 @@ public class ProdutoService {
     @Autowired
     private ProdutoRepository produtoRepository;
 
-
+    @Transactional
     public Produto save(Produto produto){
         return produtoRepository.save(produto);
+    }
+
+    public Produto buscar(Long produtoId){
+        return produtoRepository.findById(produtoId).orElseThrow();
     }
 }
