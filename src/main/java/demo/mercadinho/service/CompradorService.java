cb93965d11a84860620de1a253001062249cd06b
theJoseAlan <salesalan854@gmail.com>
2023-05-22 11:13:40 -0300

    Add buscar
    return compradorRepository.findById(compradorId).orElseThrow();

--- src/main/java/demo/mercadinho/service/CompradorService.java
+++ src/main/java/demo/mercadinho/service/CompradorService.java
@@ -5,6 +5,7 @@ import demo.mercadinho.repository.CompradorRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @AllArgsConstructor
 @Service
@@ -13,8 +14,13 @@ public class CompradorService {
     @Autowired
     private CompradorRepository compradorRepository;
 
+    @Transactional
     public Comprador salvar(Comprador comprador){
         return compradorRepository.save(comprador);
+    }
+
+    public Comprador buscar(Long compradorId){
+        return compradorRepository.findById(compradorId).orElseThrow();
     }
 
 }
