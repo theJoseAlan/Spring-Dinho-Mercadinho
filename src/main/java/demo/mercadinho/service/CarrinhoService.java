b92e43a12b8bef4d6fd861ca2c735f8810bf9ffd
theJoseAlan <salesalan854@gmail.com>
2023-05-22 11:13:07 -0300

    Update CarrinhoService.java
    Antes de salvar o carrinho, agora o método verifica se os dois objetos passados como atributo não são nulos

--- src/main/java/demo/mercadinho/service/CarrinhoService.java
+++ src/main/java/demo/mercadinho/service/CarrinhoService.java
@@ -1,10 +1,13 @@
 package demo.mercadinho.service;
 
 import demo.mercadinho.entidades.Carrinho;
+import demo.mercadinho.entidades.Comprador;
+import demo.mercadinho.entidades.Produto;
 import demo.mercadinho.repository.CarrinhoRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @AllArgsConstructor
 @Service
@@ -13,8 +16,18 @@ public class CarrinhoService {
     @Autowired
     private CarrinhoRepository carrinhoRepository;
 
-    public Carrinho save(Carrinho carrinho){
+    private CompradorService compradorService;
 
+    private ProdutoService produtoService;
+
+    @Transactional
+    public Carrinho criar(Carrinho carrinho){
+        Comprador comprador = compradorService.buscar(carrinho.getComprador().getId());
+
+        Produto produto = produtoService.buscar(carrinho.getProduto().getId());
+
+        carrinho.setComprador(comprador);
+        carrinho.setProduto(produto);
         return carrinhoRepository.save(carrinho);
     }
 
