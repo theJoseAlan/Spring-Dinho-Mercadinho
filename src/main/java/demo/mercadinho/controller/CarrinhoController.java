d4f87863b4a488f341b7cb3eac00c206da9914b2
theJoseAlan <salesalan854@gmail.com>
2023-05-22 11:09:17 -0300

    Removendo código inútil
    Havia criado um monte  de código para verificar se os objetos passados não estavam nulos antes de inserir no carrinho, mas com poucas linhas de código consegui resolver e deixar mais clean

--- src/main/java/demo/mercadinho/controller/CarrinhoController.java
+++ src/main/java/demo/mercadinho/controller/CarrinhoController.java
@@ -1,20 +1,12 @@
 package demo.mercadinho.controller;
 
 import demo.mercadinho.entidades.Carrinho;
-import demo.mercadinho.entidades.Comprador;
-import demo.mercadinho.entidades.Produto;
-import demo.mercadinho.model.StatusCompra;
 import demo.mercadinho.repository.CarrinhoRepository;
-import demo.mercadinho.repository.CompradorRepository;
-import demo.mercadinho.repository.ProdutoRepository;
 import demo.mercadinho.service.CarrinhoService;
 import lombok.AllArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import java.net.URI;
 import java.util.Optional;
 
 @AllArgsConstructor
@@ -22,58 +14,20 @@ import java.util.Optional;
 @RequestMapping("/carrinho")
 public class CarrinhoController {
 
-    
-    private CarrinhoRepository carrinhoRepository;
-
-    private final CompradorRepository compradorRepository;
-
-    private final ProdutoRepository produtoRepository;
-
     private CarrinhoService carrinhoService;
 
 
+    private CarrinhoRepository carrinhoRepository;
+
     @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
     public ResponseEntity<Carrinho> criar(@RequestBody Carrinho carrinho){
 
-        Comprador comprador = carrinho.getComprador();
-        if (comprador.getId() != null) { // Verifica se a entidade Comprador já possui ID atribuído
-            comprador = compradorRepository.findById(comprador.getId()).orElse(null); // Recupera a entidade Comprador do banco de dados
-        }
-        carrinho.setComprador(comprador); // Associa a entidade Comprador com Carrinho
-
-        Produto produto = carrinho.getProduto();
-
-        if(produto.getId() != null){
-            produto = produtoRepository.findById(produto.getId()).orElse(null);
-
-        }
-
-        carrinho.setProduto(produto);
-
-        assert produto != null;
-
-        Double valorTotal = produto.getValorUnitario()*produto.getQuantidade();
-
-        carrinho.setValorTotal(valorTotal);
-
-
-        carrinho.setStatusCompra(StatusCompra.FINALIZADA);
-
-        Carrinho carrinhoSalvo = carrinhoService.save(carrinho);
-
-        URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
-                .path("/{id}")
-                .buildAndExpand(carrinhoSalvo.getId())
-                .toUri();
-
-        return ResponseEntity.created(uri).body(carrinhoSalvo);
+        return ResponseEntity.ok().body(carrinhoService.criar(carrinho));
 
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Optional<Carrinho>> exibeporId(@PathVariable Long id){
-
+    public ResponseEntity<Optional<Carrinho>> exibir(@PathVariable Long id){
         return ResponseEntity.ok().body(carrinhoRepository.findById(id));
     }
 
