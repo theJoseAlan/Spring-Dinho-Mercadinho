e38f8daf6b80eb6a40f7f8826aa9ca619cae5347
theJoseAlan <salesalan854@gmail.com>
2023-06-10 10:32:50 -0300

    Create ObjetoNaoEncontradoException.java

---
+++ src/main/java/demo/mercadinho/exceptions/ObjetoNaoEncontradoException.java
@@ -0,0 +1,12 @@
+package demo.mercadinho.exceptions;
+
+public class ObjetoNaoEncontradoException extends Exception{
+
+    private static final long serialVersionUID = -6891662737426407991L;
+
+    public ObjetoNaoEncontradoException(String msg){
+        super(msg);
+    }
+
+
+}
